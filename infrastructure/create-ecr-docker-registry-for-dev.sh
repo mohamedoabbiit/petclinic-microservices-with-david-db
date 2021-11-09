45ba51daf1f5e3c0c888f0afd0424169761ca357
Mohamed Abbi <mohamedoabbiit@gmail.com>
2021-11-09 01:48:38 +0000

    updated the create-ecr-docker-registry-for-dev.sh

--- infrastructure/create-ecr-docker-registry-for-dev.sh
+++ infrastructure/create-ecr-docker-registry-for-dev.sh
@@ -1,5 +1,5 @@
 PATH="$PATH:/usr/local/bin"
-APP_REPO_NAME="mohamed-jenkins-repo/petclinic-app-dev"
+APP_REPO_NAME="mohamed-repo/petclinic-app-dev"
 AWS_REGION="us-east-1"
 
 aws ecr create-repository \
